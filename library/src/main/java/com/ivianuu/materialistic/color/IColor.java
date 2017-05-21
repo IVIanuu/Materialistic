ac0d7739869ac0ae85f8c41665ec1dc7bb54a166
IVIanuu <IVIanuu@gmail.com>
2017-05-21 15:29:57 +0200

    Initial commit

---
+++ library/src/main/java/com/ivianuu/materialistic/color/IColor.java
@@ -0,0 +1,12 @@
+package com.ivianuu.materialistic.color;
+
+/**
+ * Created by mikepenz on 07.07.15.
+ */
+public interface IColor {
+    String getAsString();
+
+    int getAsColor();
+
+    int getAsResource();
+}
